f0aade1c32c7f7afb09b260ff1ea47d9d845eafc
StevenGinsberg <53059396+StevenGinsberg@users.noreply.github.com>
2019-09-26 21:17:53 -0400

    Update Customer.java

--- HW1/Customer.java
+++ HW1/Customer.java
@@ -1,4 +1,46 @@
 package com.jetbrains;
 
-public class Customer {
+import java.util.Locale;
+import java.util.regex.*;
+
+public class Customer
+{
+    private int id;
+    private String fullName;
+    private String firstName, lastName;
+    private double balance;
+
+    public Customer(String name, int id,
+                    double balance)
+    {
+        Pattern p = Pattern.compile("[\\s, \\t]");
+        String[] firstLast = name.split(p.toString(), 2);
+        this.firstName = firstLast[0].replaceAll(p.toString(), "");
+        this.lastName = firstLast[1].replaceAll(p.toString(), "");
+        this.fullName = firstName + " " + lastName;
+        this.balance = balance;
+        this.id = id;
+    }
+
+    public void printInfo() {
+        java.text.NumberFormat format =
+                java.text.NumberFormat.getCurrencyInstance(Locale.US);
+        System.out.printf("%d%20s%15s%n", getId(),
+                getFullName(), format.format(getBalance()));
+    }
+
+    public int getId() {
+        return id;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getFullName() {
+        return fullName;
+    }
+    protected void setBalance(double balance) {
+        this.balance = balance;
+    }
 }

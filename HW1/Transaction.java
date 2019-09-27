8ac66cc309fd9c2b63d5e1a863d2485fa8f18b8b
StevenGinsberg <53059396+StevenGinsberg@users.noreply.github.com>
2019-09-26 21:20:43 -0400

    Update Transaction.java

--- HW1/Transaction.java
+++ HW1/Transaction.java
@@ -1,4 +1,30 @@
 package com.jetbrains;
-
 public class Transaction {
+    protected String itemName;
+    protected double price;
+    protected int quantity;
+    protected double paymentAmnt;
+    protected enum Type { ORDER, PAYMENT }
+    private Type type;
+
+    public Transaction() { }
+    public Transaction(String name, double price,
+                       int quantity)
+    {
+        this.type = Type.ORDER;
+        this.itemName = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Transaction(double paymentAmnt) {
+        this.type = Type.PAYMENT;
+        this.paymentAmnt = paymentAmnt;
+    }
+
+    public double getPrice() { return price; }
+    public String getName() { return itemName; }
+    public int getQuantity() { return quantity; }
+    public Type getType() { return type; }
+    public double getPaymentAmnt() { return paymentAmnt; }
 }
